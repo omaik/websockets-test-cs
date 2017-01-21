204f4c9e5d905480215a458f3f7234a5105299c9
Oleh Maikovych <omaik@softserveinc.com>
2017-01-21 15:44:43 +0200

    Add angularJS

---
+++ app.js
@@ -0,0 +1,5 @@
+require('angular');
+require('angular-ui-router');
+require('material-design-lite');
+
+require('./app/js/main.js');
---
+++ app/js/controllers/home/home.controller.js
@@ -0,0 +1,6 @@
+var appModule = angular.module('app')
+
+appModule.controller('homeCtrl',['$scope', function($scope) {
+  $scope.lol = 'lol';
+}]);
+
